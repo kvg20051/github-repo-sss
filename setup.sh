86fe606a1ae158ffca822898eeedf23b8c940f8d
Victor G. Krivulets <krivulets@gmail.com>
2026-09-02 23:03:07 +0300

    added alias ww md

--- setup.sh
+++ setup.sh
@@ -355,7 +355,8 @@ alias tv='"'"'terraform validate'"'"'
 alias u='"'"'sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y'"'"'
 alias y='"'"'ssh student@std-ext-010-33.praktikum-services.tech'"'"'
 alias pp='"'"'ping -c 3 ya.ru'"'"'
-alias w='"'"'watch -n 10 sudo fail2ban-client status sshd'"'"'
+alias ww='"'"'watch -n 10 sudo fail2ban-client status sshd'"'"'
+alias md='"'"'sudo mdadm --detail /dev/md0'"'"'
 
 # ========== HISTORY CONFIGURATION ==========
 
